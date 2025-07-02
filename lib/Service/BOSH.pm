ddd7c941abb331a4e214488005bd06c3e8d671c9
Dennis J. Bell <dennis.j.bell@gmail.com>
2025-07-02 10:43:55 -0700

    Fix BOSH script header removal order
    Move the script header removal regex to after the exit code
    extraction to prevent interference with exit code parsing.
    
    This ensures the command exit code is properly captured from
    the script output before cleaning up the header text.

--- lib/Service/BOSH.pm
+++ lib/Service/BOSH.pm
@@ -135,10 +135,10 @@ sub execute {
 
 	if ($opts->{interactive} && ! $opts->{passfail} && $file) {
 		$results[0] = slurp($file);
-		$results[0] =~ s/^Script [^\n]+\n//m; # remove script header (linux)
 		if ($results[0] =~ s/\nScript done.*\[COMMAND_EXIT_CODE="(.*)"]$//m) {
 			$results[1] = $1;  # Linux stores command exit code in the script output
 		}
+		$results[0] =~ s/^Script [^\n]+\n//m; # remove script header (linux)
 		$Genesis::Log::Logger->dump_var("bosh results" => \@results);
 	}
 	return !$results[1] if $opts->{passfail};
